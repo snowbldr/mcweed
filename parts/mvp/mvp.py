68241e90d92fb6d5bd5aab99ae102e614380db87
rkempton <r@snow87.com>
2025-02-20 13:30:18 -0700

    core socket tweaks for better fit and hopefully no melting

--- parts/mvp/mvp.py
+++ parts/mvp/mvp.py
@@ -23,27 +23,28 @@ class MVP:
             .add(self.core.build().translate((0, 50)))
         )
 
+wall_thickness=1.25
 
-socket = CoreSocket(
-    height=42,
-    inner_diameter=18.25,
-    fin_depth=5,
-    oring=Oring(thickness=3, outer_diameter=23, inner_diameter=17)
-)
-housing = MVPHousing(core_socket=socket, battery_holder=battery_holder_21700())
 core = McWeedCeramicFilamentPrintableCore(
     bowl=McWeedBowl(height=11, inner_diameter=15.5, outer_diameter=18),
     airway=McWeedCoreAirway(inner_diameter=1.75, turns=0.33, height=19, count=8),
     wire_ring_depth=1.75
 )
-
-mvp = MVP(
-    core=core,
-    housing=housing,
-    wall_thickness=1.25
+socket = CoreSocket(
+    height=core.height() + wall_thickness,
+    inner_diameter=core.bowl.outer_diameter + 0.25,
+    outer_diameter=24
 )
+# housing = MVPHousing(core_socket=socket, battery_holder=battery_holder_21700())
+#
+# mvp = MVP(
+#     core=core,
+#     housing=housing,
+#     wall_thickness=1.25
+# )
 
-battery_holder = mvp.housing.battery_holder.build(mvp.wall_thickness)
-battery_holder.export('battery_holder.stl')
-show_object(battery_holder)
+# mvp.housing.core_socket.build(mvp.wall_thickness).export('core_socket.stl')
 # show_object(mvp.build())
+socket = socket.build(1.25)
+socket.export('socket.stl')
+show_object(socket)
--- parts/shared/core_socket.py
+++ parts/shared/core_socket.py
@@ -3,51 +3,42 @@ from dataclasses import dataclass
 
 from cadquery import Workplane
 
-from parts.shapes.oring import Oring
-
 
 @dataclass
 class CoreSocket:
     height: float
     inner_diameter: float
-    fin_depth: float
-    oring: Oring
+    outer_diameter: float
+    vent_count: int = 8
 
-    def outer_diameter(self, wall_thickness):
-        return self.inner_diameter + wall_thickness * 2 + self.fin_depth * 2
-
-    def fin_width(self, wall_thickness):
-        return self.outer_diameter(wall_thickness) - wall_thickness * 2
-
-    def cooling_fin(self, wall_thickness: float):
+    def stop_ring(self, wall_thickness: float):
         return (
             Workplane()
-            .box(wall_thickness, self.outer_diameter(wall_thickness) - self.fin_depth, self.height, centered=True)
+            .cylinder(height=wall_thickness/2, radius=self.outer_diameter / 2)
+            .cut(
+                Workplane()
+                .cylinder(height=wall_thickness*2, radius=self.inner_diameter/2 - wall_thickness)
+            )
+            .translate((0, 0, -((self.height - wall_thickness/2)/2)))
         )
 
     def build(self, wall_thickness):
-        fin_count = math.floor(((self.fin_width(wall_thickness) * math.pi) / (wall_thickness * 2)) / 2)
-        fin_angle = 360 / fin_count
-
+        circumference = math.pi * self.inner_diameter
+        vent_angle = 360 / self.vent_count
+        vent_width = circumference / self.vent_count / 2
         tube = (
             Workplane()
-            .cylinder(height=self.height, radius=self.outer_diameter(wall_thickness) / 2)
+            .cylinder(height=self.height, radius=self.outer_diameter / 2)
             .faces('>Z')
             .workplane()
             .hole(diameter=self.inner_diameter, depth=self.height)
         )
-        for i in range(fin_count):
-            tube = tube.cut(self.cooling_fin(wall_thickness)
-                            .rotate((0, 0, 0), (0, 0, 1), i * fin_angle)
-                            .translate((0, 0, -self.oring.thickness - wall_thickness))
-                            )
-        tube = tube.cut(
-            self.oring.build()
-            .add(
+        tube.add(self.stop_ring(wall_thickness))
+        for i in range(self.vent_count):
+            tube = tube.cut(
                 Workplane()
-                .cylinder(height=self.oring.thickness, radius=self.oring.outer_diameter / 2 + wall_thickness * 0.75)
-                .translate((0, 0, self.oring.thickness * .1))
+                .box(length=wall_thickness*2.5, width=vent_width, height=self.height)
+                .translate((self.inner_diameter/2, 0))
+                .rotate((0,0,0), (0,0,1), vent_angle * i)
             )
-            .translate((0, 0, (self.height - self.oring.thickness) / 2))
-        )
         return tube
